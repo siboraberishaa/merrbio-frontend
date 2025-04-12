c6625ac752a2dfbbb1a01c648a6845ffa333b7ab
sibora github <siboraberisha@gmail.com>
2025-04-12 17:34:57 +0200

    first commit

---
+++ src/App.js
@@ -0,0 +1,40 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import RouteScrollToTop from "./helper/RouteScrollToTop";
+import HomePageOne from "./pages/HomePageOne";
+import PhosphorIconInit from "./helper/PhosphorIconInit";
+import HomePageTwo from "./pages/HomePageTwo";
+import ShopPage from "./pages/ShopPage";
+import ProductDetailsPageOne from "./pages/ProductDetailsPageOne";
+import ProductDetailsPageTwo from "./pages/ProductDetailsPageTwo";
+import CartPage from "./pages/CartPage";
+import CheckoutPage from "./pages/CheckoutPage";
+import AccountPage from "./pages/AccountPage";
+import BlogPage from "./pages/BlogPage";
+import BlogDetailsPage from "./pages/BlogDetailsPage";
+import ContactPage from "./pages/ContactPage";
+
+function App() {
+  return (
+    <BrowserRouter>
+      <RouteScrollToTop />
+      <PhosphorIconInit />
+
+      <Routes>
+        <Route exact path="/" element={<HomePageOne />} />
+        <Route exact path="/index-two" element={<HomePageTwo />} />
+        <Route exact path="/shop" element={<ShopPage />} />
+        <Route exact path="/product-details" element={<ProductDetailsPageOne />} />
+        <Route exact path="/product-details-two" element={<ProductDetailsPageTwo />} />
+        <Route exact path="/cart" element={<CartPage />} />
+        <Route exact path="/checkout" element={<CheckoutPage />} />
+        <Route exact path="/account" element={<AccountPage />} />
+        <Route exact path="/blog" element={<BlogPage />} />
+        <Route exact path="/blog-details" element={<BlogDetailsPage />} />
+        <Route exact path="/contact" element={<ContactPage />} />
+
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default App;
